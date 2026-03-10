c8c040b9017eaf01e04f9b6a529271938b9945d8
felipems999 <felipems97@hotmail.com>
2026-03-10 19:56:42 -0300

    test: :rottating_light: Implementando testes registro de usuário

--- backend/users/tests.py
+++ backend/users/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+import pytest
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+User = get_user_model()
+
+
+@pytest.mark.django_db
+def test_create_user(client):
+    payload = {
+        "username": "testuser",
+        "email": "test@example.com",
+        "password": "strongpassword123",
+    }
+    response = client.post("/api/users/register/", payload)
+    assert response.status_code == 201
+    assert User.objects.count() == 1
+    assert User.objects.get().email == "test@example.com"

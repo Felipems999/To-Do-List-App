123885eab14970053c40f0eb5d093481a3d5fdac
felipems999 <felipems97@hotmail.com>
2026-03-13 14:22:49 -0300

    feat: :sparkles: Integrando API da Google Gemini

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -10,8 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 from datetime import timedelta
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-p0c)vs)1j1r__r^h-ta+tg_o&^!%31cwxqr_zlpk@y951q)4-8"
+SECRET_KEY = os.getenv("API_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -1,5 +1,6 @@
+import os
 from rest_framework import viewsets, permissions, status
-from rest_framework.decorators import action
+from rest_framework.decorators import action, api_view, permission_classes
 from rest_framework.response import Response
 from django.db import models
 from django.contrib.auth import get_user_model
@@ -7,8 +8,45 @@ from .models import Category, Task
 from .serializer import CategorySerializer, TaskSerializer
 from .permissions import havePermission
 from .pagination import StandardResultsSetPagination
+from dotenv import load_dotenv
+from google import genai
+
+
+load_dotenv()
 
 User = get_user_model()
+
+client = genai.Client(api_key=os.getenv("GEMINI_KEY"))
+
+
+@api_view(["POST"])
+@permission_classes([permissions.IsAuthenticated])
+def suggest_subtasks(request):
+    task_title = request.data.get("title")
+    if not task_title:
+        return Response({"error": "Título é obrigatório"}, status=400)
+
+    try:
+        response = client.models.generate_content(
+            model="gemini-3-flash-preview",
+            contents=f"Com base no objetivo '{task_title}', liste 5 subtarefas curtas e práticas. Retorne apenas a lista, uma por linha, sem números e sem asteriscos.",
+        )
+
+        if response.text:
+            subtasks = [
+                line.strip()
+                for line in response.text.strip().split("\n")
+                if line.strip()
+            ]
+            return Response({"suggestions": subtasks})
+
+        return Response({"error": "A IA retornou uma resposta vazia"}, status=500)
+
+    except Exception as e:
+        print(f"Erro Gemini (Novo SDK): {e}")
+        return Response(
+            {"error": f"Erro na IA: {str(e)}"}, status=status.HTTP_502_BAD_GATEWAY
+        )
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
